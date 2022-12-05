479d8c15a9597a317de720149a94638b858168f9
Fausto Ramirez <ramirezmedia@gmail.com>
2022-12-05 18:35:10 -0500

    commit tree 882017e4578194eea6ec7268143a53d5704a07d3

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  let myArray = [];
+  let newThing = [];
+  let firstLetter, restOfWord = '';
+  
+  thing = tutorials[0].split(" ");
+  
+  for(const word of thing){
+    console.log(word)
+    
+    //separate string with first letter and rest of string. 
+    restOfWord = word.slice(1);
+    firstLetter = word[0].toUpperCase();
+    
+    
+    
+    myArray.push(firstLetter + restOfWord);
+  }
+  
+  
+  return myArray;
 }
+
+console.log(titleCased());
